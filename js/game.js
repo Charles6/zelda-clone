bea26efc08b97860bd782de183c67745fdfb7f6e
Charles Nguyen <cpnguy@gmail.com>
2017-09-23 11:07:48 -0700

    improved movement

--- js/game.js
+++ js/game.js
@@ -1,22 +1,38 @@
 const playerCreate = () => {
   var player = document.createElement("DIV");
   player.id = "playerCSS";
+  player.innerText = "L";
   document.getElementById("gameContainer").appendChild(player);
 }
 
 var startX = 0;
 var startY = 0;
 
-const move = (x,y) => {
-  if(startX >= 0 && startX <= 750){
-    startX += x;
-    document.getElementById("playerCSS").style.left = startX + "px";
+document.addEventListener('keydown', function(event) {
+  if (event.key == "ArrowUp"){
+    if(startY > 0){
+      startY -= 50;
+      document.getElementById("playerCSS").style.top = startY + "px";
+    }; 
   };
-  if(startY >= 0 && startY <= 550){
-    startY += y;
-    document.getElementById("playerCSS").style.top = startY + "px";
+  if (event.key == "ArrowRight"){
+    if(startX < 750){
+      startX += 50;
+      document.getElementById("playerCSS").style.left = startX + "px";
+    }; 
   };
-  console.log("X:"+startX+" , Y:"+startY);
-}
+  if (event.key == "ArrowDown"){
+    if(startY < 550){
+      startY += 50;
+      document.getElementById("playerCSS").style.top = startY + "px";
+    }; 
+  };
+  if (event.key == "ArrowLeft"){
+    if(startX > 0){
+      startX -= 50;
+      document.getElementById("playerCSS").style.left = startX + "px";
+    }; 
+  };
+});
 
 playerCreate();
