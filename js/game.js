25c19682062a39e1899930f3c4908710dc88fb2b
Charles Nguyen <cpnguy@gmail.com>
2017-09-24 15:55:47 -0700

    more stuff

--- js/game.js
+++ js/game.js
@@ -27,75 +27,113 @@ document.getElementById("info").appendChild(health);
 document.getElementById("info").appendChild(level);
 document.getElementById("info").appendChild(inventory);
 
-document.addEventListener('keydown', function(event) {
-  if (event.key == "ArrowUp"){
-    if(playerStat.posY > 0){
-      playerStat.posY -= 50;
-      document.getElementById("playerCSS").style.top = playerStat.posY + "px";
-    }; 
-  };
-  if (event.key == "ArrowRight"){
-    if(playerStat.posX < 750){
-      playerStat.posX += 50;
-      document.getElementById("playerCSS").style.left = playerStat.posX + "px";
-    }; 
-  };
-  if (event.key == "ArrowDown"){
-    if(playerStat.posY < 550){
-      playerStat.posY += 50;
-      document.getElementById("playerCSS").style.top = playerStat.posY + "px";
-    }; 
-  };
-  if (event.key == "ArrowLeft"){
-    if(playerStat.posX > 0){
-      playerStat.posX -= 50;
-      document.getElementById("playerCSS").style.left = playerStat.posX + "px";
-    }; 
-  };
-  
-  sameLoc();
-});
+const moveEntity = (entity,id) => {
 
+  //document.removeEventListener("keydown", keyboardBindings());
+
+  document.addEventListener('keydown', function(){
+    if (event.key == "ArrowUp"){
+      if(entity.posY > 0){
+        entity.posY -= 50;
+        document.getElementById(id).style.top = entity.posY + "px";
+      }; 
+    };
+    if (event.key == "ArrowRight"){
+      if(entity.posX < 750){
+        entity.posX += 50;
+        document.getElementById(id).style.left = entity.posX + "px";
+      }; 
+    };
+    if (event.key == "ArrowDown"){
+      if(entity.posY < 550){
+        entity.posY += 50;
+        document.getElementById(id).style.top = entity.posY + "px";
+      }; 
+    };
+    if (event.key == "ArrowLeft"){
+      if(entity.posX > 0){
+        entity.posX -= 50;
+        document.getElementById(id).style.left = entity.posX + "px";
+      }; 
+    };
+    sameLoc();
+  });
+};
+
+// const keyboardBindings = (entity,id) => {
+//     if (event.key == "ArrowUp"){
+//       if(entity.posY > 0){
+//         entity.posY -= 50;
+//         document.getElementById(id).style.top = entity.posY + "px";
+//       }; 
+//     };
+//     if (event.key == "ArrowRight"){
+//       if(entity.posX < 750){
+//         entity.posX += 50;
+//         document.getElementById(id).style.left = entity.posX + "px";
+//       }; 
+//     };
+//     if (event.key == "ArrowDown"){
+//       if(entity.posY < 550){
+//         entity.posY += 50;
+//         document.getElementById(id).style.top = entity.posY + "px";
+//       }; 
+//     };
+//     if (event.key == "ArrowLeft"){
+//       if(entity.posX > 0){
+//         entity.posX -= 50;
+//         document.getElementById(id).style.left = entity.posX + "px";
+//       }; 
+//     };
+// }
 
 const enemyList = [];
 
 const newEnemy = () => {
   var enemyStat = {
+    num:-1,
     posX:0,
     posY:0,
     hp:10,
     lv:1
   }
   const createEnemy = () => {
+    enemyStat.num++;
     enemyStat.posX = Math.floor(Math.random()*16)*50;
     enemyStat.posY = Math.floor(Math.random()*11)*50;
+
+    var enemy = document.createElement("DIV");
+    enemy.className = "enemyCSS";
+    enemy.innerText = "E";
+    enemy.id = "e"+enemyStat.num;
+    enemy.dataset.clicked = enemyStat.num;
+    //enemy.onclick = function(){ test(this); };
+    document.getElementById("board").appendChild(enemy);
+
+    document.getElementById("e"+enemyStat.num).style.top = enemyStat.posY + "px";
+    document.getElementById("e"+enemyStat.num).style.left = enemyStat.posX + "px";
+    console.log("enemy" + enemyStat.num + " x:" + enemyStat.posX + " y:" + enemyStat.posY);
+
     return enemyStat;
   }
   return createEnemy;
 }
 
-
-const placeEnemy = (index) => {
-  var enemyAdd = newEnemy();
-
-  enemyList.push(enemyAdd());
-
-  var enemy = document.createElement("DIV");
-  enemy.className = "enemyCSS";
-  enemy.innerText = "E";
-  document.getElementById("board").appendChild(enemy);
-
-  document.getElementsByClassName("enemyCSS")[index].style.top = enemyList[index].posY + "px";
-  document.getElementsByClassName("enemyCSS")[index].style.left = enemyList[index].posX + "px";
-  console.log("enemy"+ index +" x:"+enemyList[index].posX+" y:"+enemyList[index].posY);
+const test = (input) => {
+  console.log(input.id);
+  moveEntity(enemyList[input.dataset.clicked],input.id)
 }
-
 
 const sameLoc = () => {
   for(var i = 0; i <= enemyList.length-1; i++){
     if (playerStat.posX === enemyList[i].posX && playerStat.posY === enemyList[i].posY){
       playerStat.inv.push("enemy"+i);
       document.getElementById("invId").innerHTML = playerStat.inv;
+      var idRemove = "e"+i;
+      console.log(idRemove);
+      var removeItem = document.getElementById(idRemove)
+      removeItem.parentNode.removeChild(removeItem);
+      //removeThing(idRemove);
       // removeThing("enemyCSS",i);
       // console.log("List of total Enemies: "+enemyList);
       // console.log("List of collected Enemies: "+playerStat.inv);
@@ -104,17 +142,35 @@ const sameLoc = () => {
 }
 
 
-function removeThing(className,index){
-    var elements = document.getElementsByClassName(className);
-    elements[index].parentNode.removeChild(elements[index]);
+function removeThing(id){
+    var removeItem = document.getElementById("board")
+    removeItem.removeChild(id);
 }
-
 
 const startGame = () => {
   playerCreate();
-  for (var i = 0; i <= 7; i++) {
-    placeEnemy(i);
+
+  var enemyAdd = newEnemy();
+
+  for (var i = 0; i <= 10; i++) {
+
+    var testObj = {
+        num:-1,
+        posX:0,
+        posY:0,
+        hp:10,
+        lv:1
+    }
+
+    var testItem = enemyAdd();
+    console.log(testItem);
+    testObj.num = testItem.num;
+    testObj.posX = testItem.posX;
+    testObj.posY = testItem.posY;
+    enemyList.push(testObj);
+
   };
-  
+  console.log(enemyList);
+  moveEntity(playerStat,"playerCSS")
 }
 startGame();
